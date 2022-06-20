548446f744032e12f6ed171906db610e7c20736e
scottitran <44844996+scottitran@users.noreply.github.com>
2022-06-20 16:10:10 +0200

    Add files via upload

---
+++ Testing Source code/Latency_Test/Scalability_Test/parallel_server/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+import os 
+from glob import glob
+
+package_name = 'parallel_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name), glob('launch/*.launch.py')),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml'))
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='cuong1',
+    maintainer_email='cuong1@todo.todo',
+    description='TODO: Package description',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'listener = parallel_server.subscriber:main'
+        ],
+    },
+)
