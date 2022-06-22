77f6ea5dff2f3987727d290857276c7325075235
scottitran <44844996+scottitran@users.noreply.github.com>
2022-06-22 13:30:34 +0200

    Add files via upload

---
+++ Testing Source code/Latency_Test/Publisher_Frequency_Test/sender_ros2/launch/sender_ros2.launch.py
@@ -0,0 +1,25 @@
+# from distutils.command.config import config
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    config = os.path.join(
+        get_package_share_directory('sender_ros2'),
+        'config',
+        'params.yaml'
+        )
+    
+    node_1 = Node(
+            package='sender_ros2',
+            # namespace='sub1',
+            executable='talker',
+            name='sender',
+            parameters=[config]    
+        )
+ 
+    ld.add_action(node_1)
+    return ld
